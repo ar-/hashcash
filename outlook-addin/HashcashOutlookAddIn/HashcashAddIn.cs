93377233ab423223e50662fad56110ac312bc704
Andreas <andreas.redmer@cashconverters.com>
2016-11-15 13:21:08 +0800

    added header reader class (no writer yet)

--- outlook-addin/HashcashOutlookAddIn/HashcashAddIn.cs
+++ outlook-addin/HashcashOutlookAddIn/HashcashAddIn.cs
@@ -2,12 +2,46 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Xml.Linq;
 using Outlook = Microsoft.Office.Interop.Outlook;
 using Office = Microsoft.Office.Core;
 
 namespace HashcashOutlookAddIn
 {
+    public static class MailItemExtensions
+    {
+        private const string HeaderRegex =
+            @"^(?<header_key>[-A-Za-z0-9]+)(?<seperator>:[ \t]*)" +
+                "(?<header_value>([^\r\n]|\r\n[ \t]+)*)(?<terminator>\r\n)";
+        private const string TransportMessageHeadersSchema =
+            "http://schemas.microsoft.com/mapi/proptag/0x007D001E";
+
+        public static string[] Headers(this Outlook.MailItem mailItem, string name)
+        {
+            var headers = mailItem.HeaderLookup();
+            if (headers.Contains(name))
+                return headers[name].ToArray();
+            return new string[0];
+        }
+
+        public static ILookup<string, string> HeaderLookup(this Outlook.MailItem mailItem)
+        {
+            var headerString = mailItem.HeaderString();
+            var headerMatches = Regex.Matches
+                (headerString, HeaderRegex, RegexOptions.Multiline).Cast<Match>();
+            return headerMatches.ToLookup(
+                h => h.Groups["header_key"].Value,
+                h => h.Groups["header_value"].Value);
+        }
+
+        public static string HeaderString(this Outlook.MailItem mailItem)
+        {
+            return (string)mailItem.PropertyAccessor
+                .GetProperty(TransportMessageHeadersSchema);
+        }
+    }
+
     public partial class HashcashAddIn
     {
         Outlook.Inspectors inspectors;
@@ -21,6 +55,14 @@ namespace HashcashOutlookAddIn
                 {
                     mailItem.Subject = "This text was added by using code";
                     mailItem.Body = "This text was added by using code";
+                    mailItem.PropertyAccessor.GetProperty("PR_TRANSPORT_MESSAGE_HEADERS");
+                    string[] preparedByArray = mailItem.Headers("From");
+                    string preparedBy;
+                    if (preparedByArray.Length == 1)
+                        preparedBy = preparedByArray[0];
+                    else
+                        preparedBy = "no one";
+                    mailItem.Body += preparedBy;
                 }
 
             }
